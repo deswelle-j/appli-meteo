fe39a844aa42b7c006707d3978ffd946903b37f1
deswelle-j <deswelle.johan@gmail.com>
2018-02-16 20:35:56 +0100

    Change weather lang to fr and change properties

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -12,7 +12,7 @@ export class WeatherProvider {
   }
   getWeather (city){
     // http://api.openweathermap.org/data/2.5/weather?q=Lille,fr&appid=6c3ed53824c8e24f514e5cf4e43ee3af
-    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q='+ city +',fr&appid='+this.apiKey)
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q='+ city +',fr&lang=fr&appid='+this.apiKey)
     .toPromise();
   }
 
